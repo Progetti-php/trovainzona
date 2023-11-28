e9f7cd13aed42fa4841c12b81e32b76a70ceb701
Valeria Saponaro <v.saponaro97@gmail.com>
2023-11-28 10:14:07 +0100

    pagina dettaglio

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -6,10 +6,7 @@
             <div class="col-7 h-100">
                 <h1 class="text-center">Titolo</h1>
                 <p class="fs-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis enim, quidem exercitationem aliquid tempora sapiente possimus hic illum ea ab in aliquam earum reprehenderit maxime labore vel omnis, dolor laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi aperiam animi fugit, nemo harum itaque ducimus vitae omnis blanditiis dignissimos? Laboriosam at, quam magni temporibus porro commodi voluptate tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusamus reiciendis, doloribus perspiciatis suscipit aliquam unde quo nam, repudiandae culpa mollitia voluptates error? Eum odio delectus soluta ullam inventore cumque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore laborum et perspiciatis ab atque accusamus laudantium aspernatur! Minima iste accusantium minus magnam omnis, aperiam repellendus neque debitis, asperiores, laborum consequuntur. Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
-                <div class="d-flex justify-content-end align-items-end m-3">
-                    <a href="{{route('detail')}}" type="button" class="btn bgP colorA">Scopri di più</a>
-                    
-                </div>
+                
                 
             </div>
             <div class="col-5">
@@ -51,10 +48,7 @@
             <div class="col-7 h-100">
                 <h1 class="text-center">Titolo</h1>
                 <p class="fs-4 text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis enim, quidem exercitationem aliquid tempora sapiente possimus hic illum ea ab in aliquam earum reprehenderit maxime labore vel omnis, dolor laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae eligendi aperiam animi fugit, nemo harum itaque ducimus vitae omnis blanditiis dignissimos? Laboriosam at, quam magni temporibus porro commodi voluptate tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus accusamus reiciendis, doloribus perspiciatis suscipit aliquam unde quo nam, repudiandae culpa mollitia voluptates error? Eum odio delectus soluta ullam inventore cumque. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore laborum et perspiciatis ab atque accusamus laudantium aspernatur! Minima iste accusantium minus magnam omnis, aperiam repellendus neque debitis, asperiores, laborum consequuntur. Lorem ipsum, dolor sit amet consectetur adipisicing elit.  </p>
-                <div class="d-flex justify-content-end align-items-end m-3">
-                    <a href="{{route('detail')}}" type="button" class="btn bgP colorA">Scopri di più</a>
-                    
-                </div>
+                
                 
             </div>
             
